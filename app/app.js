dc333b121209cd3d443a99b9d9fe52da5c992cf1
kylesaldana5 <kylesaldana5@gmail.com>
2018-01-31 13:59:52 -0600

    added the links on the home page / route listing completed

--- app/app.js
+++ app/app.js
@@ -4,10 +4,13 @@ const app = angular.module("routes", ["ngRoute"])
     app.config($routeProvider =>{
         $routeProvider
         .when('/', {
+            templateUrl: "app/partials/list.html"
+        })
+        .when('/65', {
             templateUrl: "app/partials/highway1.html",
             controller: "highway1Ctrl"
         })
-        .when('/next', {
+        .when('/70', {
             templateUrl:"app/partials/highway2.html",
             controller: "highway2Ctrl"
         })
