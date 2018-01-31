4bbb1d8abd9f6b9d97f2e96f6e5baa68bb2dfa92
kylesaldana5 <kylesaldana5@gmail.com>
2018-01-31 13:27:07 -0600

    finshed route routing /  will now add links for Route Listing

---
+++ app/controllers/highway1Ctrl.js
@@ -0,0 +1,6 @@
+"use strict";
+
+app.controller("highway1Ctrl", function ($scope,) {
+    $scope.highwayName = "I-65";
+    $scope.highwayDescription = "Take me South"
+})
---
+++ app/controllers/highway2Ctrl.js
@@ -0,0 +1,6 @@
+"use strict";
+
+app.controller("highway2Ctrl", function ($scope, ) {
+    $scope.highwayName = "I-70";
+    $scope.highwayDescription = "Take me West"
+})
